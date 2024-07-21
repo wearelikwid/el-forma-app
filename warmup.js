59a7caa530d819e933a43bca295ad17c71feeba7
hussein <86411714+wearelikwid@users.noreply.github.com>
2024-07-21 08:03:55 +0200

    Update warmup.js

--- warmup.js
+++ warmup.js
@@ -1,16 +1,15 @@
-// warmup.js
-
 import { warmups } from './exercises.js';
 import { shuffleArray } from './utils.js';
 
 export function generateWarmup() {
-    let warmup = '<h3>Warm-up (5-10 minutes)</h3>';
+    let warmup = '<h3 class="section-title">Warm-up (5-10 minutes)</h3><ul class="exercise-list">';
     const shuffledWarmups = [...warmups];
     shuffleArray(shuffledWarmups);
     
     for (let i = 0; i < 5; i++) {
-        warmup += `<p>${shuffledWarmups[i][0]}: ${shuffledWarmups[i][1]}</p>`;
+        warmup += `<li><span class="exercise-name">${shuffledWarmups[i][0]}:</span> ${shuffledWarmups[i][1]}</li>`;
     }
     
+    warmup += '</ul>';
     return warmup;
 }
